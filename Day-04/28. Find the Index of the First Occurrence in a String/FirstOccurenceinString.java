97ffd385a29861b61e064f802eda36c06e87526c
Sayyad Aslam <sayyadaslam2020@gmail.com>
2024-07-26 22:55:59 +0530

    Created FirstOccurenceinString.java

---
+++ Day-04/28. Find the Index of the First Occurrence in a String/FirstOccurenceinString.java
@@ -0,0 +1,17 @@
+public class FirstOccurenceinString {
+    public int strStr(String haystack, String needle) {
+        int n1=haystack.length();
+        int n2=needle.length();
+        if(n1<n2)
+            return -1;
+        for(int i=0;i<=n1-n2;i++){
+            int j=0;
+            while(j<n2 && haystack.charAt(i+j)==needle.charAt(j))
+                j++;
+            if(j==n2){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
