2dd33815c62242c707536b5e8e7645768c8fc686
Sayyad Aslam <sayyadaslam2020@gmail.com>
2024-07-30 23:27:44 +0530

    Created findDuplicate.java

---
+++ Day-08/287. Find the Duplicate Number/findDuplicate.java
@@ -0,0 +1,13 @@
+public class findDuplicate {
+    public int findDuplicate(int[] nums) {
+
+        HashSet<Integer> st = new HashSet<>();
+        for (int num : nums) {
+            if (st.contains(num)) {
+                return num;
+            }
+            st.add(num);
+        }
+        return -1;
+    }
+}
